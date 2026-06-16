3588937b6ed4cb618335d31529d541d9808a188d
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-16 09:30:28 +0200

    primera subida

---
+++ react-netlify/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+
+    <>
+      <></>
+
+      <div className='container'>
+        <h1>Publicación web React en Netlify con github</h1>
+      </div>
+
+    </>
+    
+  )
+}
+
+export default App
