d7ee16aa3dc8e619ae71eb595d752bdfcf97cea7
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-16 09:55:24 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <></>
 
       <div className='container'>
-        <h1>Publicación web React en Netlify con github</h1>
+        <h1>Publicación web React en Netlify con github - actualizado</h1>
       </div>
 
     </>
